8ec15a3a260d5b8e5f6e37816a5164c0b5dc528a
Yuanyuan Yang <1005842457@qq.com>
2024-02-02 16:57:24 -0500

    Update manipulatedom.js

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -21,11 +21,36 @@ function createTxtNode(txt) {
   return txtNode;
 }
 
+function createButtonNode(rowNum) {
+  let btnNode = document.createElement("button");
+  btnNode.textContent = "Edit text";
+  btnNode.onclick = function() {
+    let parentCell = this.parentNode; // td element
+    let txtInput = document.createElement('input');
+    txtInput.type = 'text';
+    txtInput.value = parentCell.previousSibling.textContent;
+    parentCell.previousSibling.textContent = '';
+    parentCell.previousSibling.appendChild(txtInput);
+    txtInput.focus();
+    this.textContent = 'Save';
+    this.onclick = function() {
+      let newTxt = txtInput.value;
+      parentCell.previousSibling.textContent = newTxt;
+      parentCell.removeChild(txtInput);
+      this.textContent = 'Edit text';
+      this.onclick = createButtonNode(rowNum).onclick;
+    };
+  };
+  return btnNode;
+}
+
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    let editBtn = createButtonNode(i);
+    let col2 = createTDNode(editBtn);
+    tableNode.appendChild(createTRNode([col1, col2]));
   }
   document.getElementById("root").appendChild(tableNode);
 }
