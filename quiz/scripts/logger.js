e3d4ddf0de4562335271cc764e031d0644b6412a
Yuanyuan Yang <1005842457@qq.com>
2024-02-02 16:09:26 -0500

    Update logger.js

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,14 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+function logMsg(obj) {
+  if (obj && obj.hasOwnProperty('errMsg')) {
+    console.error(obj.errMsg);
+  } else {
+    console.log("The object does not have an 'errMsg' property.");
+  }
+}
+
+const exObj = {
+  errMsg: 'This is an error message.'
+};
+
+logMsg(exObj);
